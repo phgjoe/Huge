cf66755c144d7bf6834afadcaa816a7de46c2a75
Joe McConnell <joe@pinckneyhugo.com>
2015-01-30 11:44:36 -0500

    First Push
    I'm so excited

---
+++ www/js/main.js
@@ -0,0 +1,29 @@
+jQuery('body').css('height', window.innerHeight);
+
+jQuery('#huge').on('click tap touch', function(){
+
+    jQuery('audio').each(function() {
+        jQuery(this).get(0).pause();
+    });
+
+    randomHuge = Math.floor(Math.random() * 10) + 1;
+    jQuery('.hugeaudio' + randomHuge).get(0).currentTime = 0;
+    jQuery('.hugeaudio' + randomHuge).get(0).play();
+
+    //Animation can add distances to pre-defined properties
+    jQuery(this).find('img').animate({
+        width: '-=30px',
+        height: '-=39px',
+        }, 100, 'swing', function() {
+        // function code on animation complete
+    });
+
+    jQuery(this).delay(100).find('img').animate({
+        width: '+=30px',
+        height: '+=39px',
+        }, 1000, 'easeOutElastic', function() {
+        // function code on animation complete
+    });
+
+    return false;
+});

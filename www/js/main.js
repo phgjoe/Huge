ad40bbbf5456453038a092e29a674fa63e52f055
Joe McConnell <joe@pinckneyhugo.com>
2015-02-05 17:12:50 -0500

    Shake11

--- www/js/main.js
+++ www/js/main.js
@@ -28,42 +28,63 @@ jQuery('#huge').on('click tap touch', function(){
     return false;
 });
 
+// //wait for PhoneGap to load
+// document.addEventListener("deviceready", loaded, false);
+ 
+// // PhoneGap is ready
+// function loaded() {
+//     startWatch();
+// }
+ 
+// // Start watching the acceleration
+ 
+// function startWatch() {
+ 
+//     // Update acceleration every 3 seconds
+//     var options = { frequency: 3000 };
+ 
+//     watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+// }
+ 
+// // Stop watching the acceleration
+// function stopWatch() {
+//     if (watchID) {
+//         navigator.accelerometer.clearWatch(watchID);
+//         watchID = null;
+//     }
+// }
+ 
+// // Success
+// function onSuccess(acceleration) {
+//     var element = document.getElementById('accelerometer');
+//     element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+//                         'Acceleration Y: ' + acceleration.y + '<br />' +
+//                         'Acceleration Z: ' + acceleration.z + '<br />' +
+//                         'Timestamp: '      + acceleration.timestamp + '<br />';
+// }
+ 
+//  // Error
+// function onError() {
+//     alert('onError!');
+// }
+
 //wait for PhoneGap to load
-document.addEventListener("deviceready", loaded, false);
- 
-// PhoneGap is ready
-function loaded() {
-    startWatch();
-}
- 
-// Start watching the acceleration
- 
-function startWatch() {
- 
-    // Update acceleration every 3 seconds
-    var options = { frequency: 3000 };
- 
-    watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-}
- 
-// Stop watching the acceleration
-function stopWatch() {
-    if (watchID) {
-        navigator.accelerometer.clearWatch(watchID);
-        watchID = null;
+document.addEventListener("deviceready", startShaking, false);
+
+function startShaking(){
+    var myShakeEvent = new Shake({
+        threshold: 15, // optional shake strength threshold
+        timeout: 1000 // optional, determines the frequency of event generation
+    });
+
+    myShakeEvent.start();
+
+    window.addEventListener('shake', shakeEventDidOccur, false);
+
+    //function to call when shake occurs
+    function shakeEventDidOccur () {
+
+        //put your own code here etc.
+        alert('shake!');
     }
-}
- 
-// Success
-function onSuccess(acceleration) {
-    var element = document.getElementById('accelerometer');
-    element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
-                        'Acceleration Y: ' + acceleration.y + '<br />' +
-                        'Acceleration Z: ' + acceleration.z + '<br />' +
-                        'Timestamp: '      + acceleration.timestamp + '<br />';
-}
- 
- // Error
-function onError() {
-    alert('onError!');
 }

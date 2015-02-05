8932661c7ad87cc17144a4a015a6d4617485b34e
Joe McConnell <joe@pinckneyhugo.com>
2015-02-05 15:58:49 -0500

    Shake5

--- www/js/main.js
+++ www/js/main.js
@@ -31,11 +31,38 @@ jQuery('#huge').on('click tap touch', function(){
 
 // Wait for Cordova to connect with the device
 //
-document.addEventListener("deviceready",onDeviceReady,false);
+// document.addEventListener("deviceready",onDeviceReady,false);
+// function onDeviceReady() {
+//     shake.startWatch(shaker);
+// }
+
+// function shaker(){
+//     alert('shake');
+// }
+
+
+
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// device APIs are available
+//
 function onDeviceReady() {
-    shake.startWatch(shaker);
+    navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
 }
 
-function shaker(){
-    alert('shake');
+// onSuccess: Get a snapshot of the current acceleration
+//
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+// onError: Failed to get the acceleration
+//
+function onError() {
+    alert('onError!');
 }

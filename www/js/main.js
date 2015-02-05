379d0e4aeeb9843aee276f13a93163271345cccc
Joe McConnell <joe@pinckneyhugo.com>
2015-02-05 15:40:54 -0500

    Accel Take 2

--- www/js/main.js
+++ www/js/main.js
@@ -27,3 +27,28 @@ jQuery('#huge').on('click tap touch', function(){
 
     return false;
 });
+
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// device APIs are available
+//
+function onDeviceReady() {
+    navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+}
+
+// onSuccess: Get a snapshot of the current acceleration
+//
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+// onError: Failed to get the acceleration
+//
+function onError() {
+    alert('onError!');
+}
